390492fc63ea5c06889ebf82dea432de473f0a51
abdullah-fikri <abdullahfikri981@gmail.com>
2025-09-10 23:36:22 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const rl = readline.createInterface({
 // 
 
 
-const convert = require('./lib/converter');
-const manual = require('./lib/manual');
+// const convert = require('./lib/converter');
+// const manual = require('./lib/manual');
+// const {convertAsync} = require('./lib/asyncronous/convertAsync');
+// const { manual } = require('./lib/manual');
+const { manualAsync } = require('./lib/asyncronous/manualAsync');
 
 
-rl.question('input date! ', (input) => {
-
+rl.question('input date! ', async (input) => {
+  try {
+    // await convertAsync(input);
+    await manualAsync(input);
+  } catch (error) {
+    console.log(error);
+  }
     
-    convert.convert(input);
-    // manual.manual(input)
+    // convert.convert(input);
+    // manual.manual(input);
+
     rl.close();
 
 });
